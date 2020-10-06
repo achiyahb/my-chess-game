58835e9de9b632d91a7f426f7421a5c0324547a1
Yair Cohen <yaircohenth@gmail.com>
2020-10-06 06:20:16 +0300

    1. Added better namings 2. Remove inline style, it's considered as an anti pattern, bad practice for performance and code maintainability.

--- src/views/ChessGame.vue
+++ src/views/ChessGame.vue
@@ -1,32 +1,33 @@
 <template>
-  <div>
-    <h3>{{whiteCurrentTurn?'white turn':'black turn'}}</h3>
-  <div class='board' style="width: 40rem;">
-    <div v-for="(row,x) of board">
-      <div v-for="(square,y) of row">
-        <div
-            :class="!square.mark ? ((x+y)%2 === 0 ? 'white':'green') : ((x+y)%2 === 0 ? 'grab':'darkGrab' )"
-            :style="`width: 5rem; height:5rem;`"
-        >
-          <img v-if="square.piece" :src="`https://images.chesscomfiles.com/chess-themes/pieces/neo/100/${square.piece.color}${square.piece.letter}.png`"
-               :style="`width: 5rem; height:5rem;`"
-               @click="grab(square)"
-               class="piece"
-               alt="piece"
-          />
-          <div v-if="square.canGo"
-            :class="!square.mark ? ((x+y)%2 === 0 ? 'white':'green') : ((x+y)%2 === 0 ? 'grab':'darkGrab' )"
-            :style="`width: 5rem; height:5rem;`"
-               @click="move(square)"
+  <div class="container">
+    <h3>{{ whiteCurrentTurn ? 'white turn' : 'black turn' }}</h3>
+    <div class='board'>
+      <div v-for="(row,x) of board">
+        <div v-for="(square,y) of row">
+          <div
+              :class="!square.mark ? ((x+y)%2 === 0 ? 'white':'green') : ((x+y)%2 === 0 ? 'grab':'darkGrab' )"
+              :style="`width: 5rem; height:5rem;`"
           >
-            <div
-                 id="circle"
+            <img v-if="square.piece"
+                 :src="`https://images.chesscomfiles.com/chess-themes/pieces/neo/100/${square.piece.color}${square.piece.letter}.png`"
+                 :style="`width: 5rem; height:5rem;`"
+                 @click="grab(square)"
+                 class="piece"
+                 alt="piece"
             />
+            <div v-if="square.canGo"
+                 :class="!square.mark ? ((x+y)%2 === 0 ? 'white':'green') : ((x+y)%2 === 0 ? 'grab':'darkGrab' )"
+                 :style="`width: 5rem; height:5rem;`"
+                 @click="move(square)"
+            >
+              <div
+                  id="circle"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -38,22 +39,22 @@ import diagonally from "@/movement/diagonally";
 
 export default {
   data: () => ({
-    whiteSquareMarked : [],
-    blackSquareMarked : [],
+    whiteSquareMarked: [],
+    blackSquareMarked: [],
     board: [],
     canGoArray: [],
     whiteCurrentTurn: true
   }),
-  methods:{
-    grab(square){
+  methods: {
+    grab(square) {
       let isItWhite = square.piece.color === 'w'
-      if (square.canGo){
+      if (square.canGo) {
         if (this.whiteCurrentTurn === isItWhite) return
         this.move(square)
         this.cantGo()
         return
       }
-      if (square.mark){
+      if (square.mark) {
         this.unmarkTheSquare()
         this.cantGo()
         this.$forceUpdate()
@@ -64,38 +65,38 @@ export default {
       this.whereCanGo(square)
       this.$forceUpdate()
     },
-    markTheSquare(square){
+    markTheSquare(square) {
       square.mark = true
       this.unmarkTheSquare()
       this.whiteSquareMarked.push(square)
     },
-    unmarkTheSquare(){
-      while(this.whiteSquareMarked.length){
+    unmarkTheSquare() {
+      while (this.whiteSquareMarked.length) {
         this.whiteSquareMarked[0].mark = false
         this.whiteSquareMarked.shift()
       }
     },
-    whereCanGo(square){
+    whereCanGo(square) {
       let responseArray
       this.cantGo()
       let piece = square.piece
       let i = piece.color === 'w' ? -1 : 1
       let movement = piece.movement
-      if (movement[0] === "forward"){
+      if (movement[0] === "forward") {
         responseArray = pawn.pawnCanGoTO(square, i, this.board)
       }
-      if (movement[0] === 'diagonally'){
+      if (movement[0] === 'diagonally') {
         responseArray = diagonally.diagonallyCanGoTO(square, this.board)
       }
       this.canGoArray = responseArray[0]
       this.board = responseArray[1]
     },
-    cantGo(){
-      for (let square of this.canGoArray){
+    cantGo() {
+      for (let square of this.canGoArray) {
         square.canGo = false
       }
     },
-    move(square){
+    move(square) {
       square.mark = true
       this.whiteSquareMarked.push(square)
       square.piece = this.whiteSquareMarked[0].piece
@@ -106,38 +107,53 @@ export default {
     },
   },
   created() {
-   this.board = setTheBoard.setTheBoard()
+    this.board = setTheBoard.setTheBoard()
     console.log(this.board)
   }
 }
 </script>
 
 <style>
-.board{
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.board {
   display: grid;
   grid-template-columns: repeat(8, 1fr);
+  width: 40rem;
 }
-.white{
+
+.white {
   background-color: #ede9ca;
 }
-.green{
+
+.green {
   background-color: #707a30;
 }
-.grab{
+
+.grab {
   background-color: #f5e263;
 }
-.darkGrab{
+
+.darkGrab {
   background-color: #b6ad2a;
 }
-.lastMove{
+
+.lastMove {
   background-color: #f5e263;
 }
-.lastPosition{
+
+.lastPosition {
   background-color: #b6ad2a;
 }
-.piece{
+
+.piece {
   cursor: grab;
 }
+
 #circle {
   width: 32px;
   height: 32px;
