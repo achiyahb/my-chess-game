d688913ae9002796fa62c02f296833c2875d6867
achiyahb <achiyahb@gmail.com>
2020-10-19 05:30:02 +0300

    create knight threatening check

--- src/tools/checkThreatning.js
+++ src/tools/checkThreatning.js
@@ -18,6 +18,9 @@ function checkHowThreatOnEachSquare(board){
         }
     }
     console.log(knightSquaresToCheck)
+    knightSquaresToCheck.forEach(square => {
+        knightCheck(square, board)
+    })
 }
 
 function straightCheck(square,board){
@@ -46,13 +49,7 @@ function checkSideSquare(square,board,i,j, piecesToCheck,checkSquare){
     let sideSquare = board[square.x+j][square.y+i]
     if (sideSquare.piece){
         if (piecesToCheck.includes(sideSquare.piece.letterName)){
-            let threatenPiece = {
-                pieceColor: sideSquare.piece.color,
-                pieceLetter: sideSquare.piece.letterName,
-                locationX: sideSquare.x ,
-                locationY: sideSquare.y
-            }
-            checkSquare.threatBy.push(threatenPiece)
+            declareOnThreaten(sideSquare,checkSquare)
         }
     } else {
         if(start){
@@ -61,4 +58,32 @@ function checkSideSquare(square,board,i,j, piecesToCheck,checkSquare){
         }
         checkSideSquare(sideSquare,board,i,j,piecesToCheck,checkSquare)
     }
+}
+
+function knightCheck(square, board){
+    for (let i = -2; i<3; i+=4){
+        for (let j = 0; j<2; j++){
+            for (let h = -1; h<2; h+=2) {
+                let a = j === 0 ? i : h
+                let b = j === 1 ? i : h
+                checkKnightNextMoveSquare(square, board, a, b)
+            }
+        }
+    }
+}
+
+function checkKnightNextMoveSquare(square,board,i,j){
+    if (square.x+j > 7 || square.x+j < 0 || square.y+i > 7 || square.y+i < 0) return
+    let sideSquare = board[square.x+j][square.y+i]
+    declareOnThreaten(square,sideSquare)
+}
+
+function declareOnThreaten(threateningSquare,threatenedSquare){
+    let threatenPiece = {
+        pieceColor: threateningSquare.piece.color,
+        pieceLetter: threateningSquare.piece.letterName,
+        locationX: threateningSquare.x,
+        locationY: threateningSquare.y
+    }
+    threatenedSquare.threatBy.push(threatenPiece)
 }
